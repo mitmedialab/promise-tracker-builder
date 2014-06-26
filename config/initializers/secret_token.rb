6558beab9a1d99826473f8b1c29fe1cf32ce4cf8
emreiser <emilie.reiser@gmail.com>
2014-06-26 17:40:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PromiseTracker::Application.config.secret_key_base = '0e2dcc999ae066653ecb90673533078a21e7af90d9021af27e22d508b5c0e7f5b56153095494d54e53222fd6a369f2a58b6e7c6e778ec7fb577b87d08aff7862'
