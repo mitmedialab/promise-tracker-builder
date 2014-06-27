dc5cdaa5cda283c5945039a584fa6ebb57392ba8
emreiser <emilie.reiser@gmail.com>
2014-06-27 09:36:53 -0400

    Add Form model, controller, views and routes.

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,24 @@
+class FormsController < ApplicationController
+  def index
+  end
+
+  def new
+    @form = Form.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/views/forms/new.html.erb
@@ -0,0 +1,1 @@
+<h1>Create a new form</h1>
