62dc418e4d13d0ee728f60147160bcadaf23b70e
emreiser <emilie.reiser@gmail.com>
2014-07-09 18:33:39 -0400

    Implement mobile survey preview

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ PromiseTracker::Application.routes.draw do
   devise_for :users
   root to: 'home#index'
 
+  get '/surveys/:id/preview', to: 'surveys#preview', as: 'preview_survey'
+
   resources :users
   resources :surveys do
     resources :inputs
