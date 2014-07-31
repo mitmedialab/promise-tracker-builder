3c9a95f9fe2c0975aec0dedd862d94d0f420779c
emreiser <emilie.reiser@gmail.com>
2014-07-31 13:58:02 -0400

    Implement preview toggle. Localize survey builder text

--- app/assets/javascripts/surveyPreview.js
+++ app/assets/javascripts/surveyPreview.js
@@ -5,10 +5,17 @@ PT.nextInput = function(event){
   var direction = $(event.currentTarget).attr("data-direction");
 
   if(direction === "right" && index < PT.survey.inputs().length - 1){
-    debugger
-      PT.selectedInput(PT.survey.inputs()[index + 1]);
+    PT.selectedInput(PT.survey.inputs()[index + 1]);
   } else if(direction === "left" && index > 0){
-    debugger
-      PT.selectedInput(PT.survey.inputs()[index - 1]);
+    PT.selectedInput(PT.survey.inputs()[index - 1]);
+  }
+};
+
+PT.togglePreview = function(event){
+  $(".preview-container").slideToggle();
+  if($(this).html() === PT.flash.hide_preview){
+    $(this).html(PT.flash.show_preview);
+  } else {
+    $(this).html(PT.flash.hide_preview);
   }
 };
