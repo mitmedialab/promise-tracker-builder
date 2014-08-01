59bb5578dc7deeb4c3de1769755054a9b82ce63a
emreiser <emilie.reiser@gmail.com>
2014-08-01 11:36:33 -0400

    Add validations for survey and input

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,9 @@ class Survey < ActiveRecord::Base
   has_many :inputs
   belongs_to :user
 
+  validates :title, presence: true
+  validates :status, presence: true
+
   STATUS =  ["editing", "active", "closed"]
 
 end
