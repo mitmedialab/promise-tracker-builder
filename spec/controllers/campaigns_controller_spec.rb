a1528d83f5c757ccc4ce56fc4a673e819e932e87
emreiser <emilie.reiser@gmail.com>
2014-08-06 14:19:21 -0400

    Add campaign controller, routes and views

---
+++ spec/controllers/campaigns_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe CampaignsController do
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET 'create'" do
+    it "returns http success" do
+      get 'create'
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "returns http success" do
+      get 'edit'
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET 'update'" do
+    it "returns http success" do
+      get 'update'
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET 'destroy'" do
+    it "returns http success" do
+      get 'destroy'
+      expect(response).to be_success
+    end
+  end
+
+end
