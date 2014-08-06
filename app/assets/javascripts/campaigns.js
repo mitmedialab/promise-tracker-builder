beeee651fa82618f16ce2c6061c54eff10cdff54
emreiser <emilie.reiser@gmail.com>
2014-08-06 15:55:52 -0400

    Implement goals definition page

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var PT = PT || {};
+
+PT.validateInputs = function($section){
+  var inputs = Array.prototype.slice.call($section.find("textarea, input"));
+  var emptyCount = 0;
+
+  inputs.forEach(function(input){
+    input.value === "" ? emptyCount += 1 : emptyCount;
+  });
+
+  return emptyCount;
+};
