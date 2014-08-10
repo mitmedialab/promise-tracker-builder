a120fbd8d6884a7238f25d2a8c3107c9408923a3
emreiser <emilie.reiser@gmail.com>
2014-08-10 19:15:12 -0400

    Add survey outline to campaign show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user! 
 
   def show
-    @campaigns = current_user.campaigns
+    @campaigns = current_user.campaigns.sort_by(&:status)
   end
 
 end
