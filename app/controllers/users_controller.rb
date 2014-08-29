3acf56d12a694eaf131aa7c96be2241d04c9f41a
emreiser <emilie.reiser@gmail.com>
2014-08-29 11:23:28 -0400

    Style devise error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user! 
 
+  def index
+    redirect_to root_path
+  end
+
   def show
     @campaigns = current_user.campaigns.sort_by(&:status)
   end
