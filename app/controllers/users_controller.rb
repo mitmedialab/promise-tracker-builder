cbb86fb1a353b0bfe6b56f308015f7013e4bb62e
emreiser <emilie.reiser@gmail.com>
2014-09-02 14:14:19 -0400

    Add edit user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,24 @@ class UsersController < ApplicationController
     @campaigns = current_user.campaigns.sort_by(&:status)
   end
 
+  def edit
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = t("defaults.validations.confirm_save")
+      redirect_to user_path(current_user)
+    else
+      flash.now[:error] = current_user.errors.full_messages.to_sentence
+      render action: 'edit'
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:bio, :email, :username)
+  end
+
 end
