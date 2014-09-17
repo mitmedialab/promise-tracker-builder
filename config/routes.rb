3b7594754fd5d72a39194857b326de1d4c960547
emreiser <emilie.reiser@gmail.com>
2014-09-17 13:03:54 -0400

    Implement API controller for campaigns

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,10 @@ PromiseTracker::Application.routes.draw do
       resources :inputs
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :campaigns, only: [:index, :show]
+    end
+  end
 end
