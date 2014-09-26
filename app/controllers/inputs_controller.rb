456bfb3f94395c4dc9fc6bf03372eeb5bc4b85b8
emreiser <emilie.reiser@gmail.com>
2014-09-26 16:56:33 -0400

    Switch input options to array of strings

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -3,11 +3,15 @@ class InputsController < ApplicationController
   def create
     # survey = Survey.find(params[:input][:survey_id])
     # input = params[:input]
-    # binding.pry
 
     @input = Input.find_or_create_by(id: params[:id])
     @input.update_attributes(input_params)
     @input.guid = make_guid(@input.label, @input.id)
+    if @input.input_type == 'select' || @input.input_type == 'select1'
+      @input.update_attribute(:options, params[:options]) 
+    else
+      @input.update_attribute(:options, [])
+    end
 
     @input.save
 
