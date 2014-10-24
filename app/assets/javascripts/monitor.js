51fbe4a507ab35c831fbb04909ec17564fb6b0bf
emreiser <emilie.reiser@gmail.com>
2014-10-23 22:19:50 -0400

    Add activity graph to monitor page

--- app/assets/javascripts/monitor.js
+++ app/assets/javascripts/monitor.js
@@ -6,168 +6,109 @@ PT.retrieveResponses = function(surveyId){
   var url = PT.aggregatUrl + "/surveys/" + surveyId + "/responses";  
   $.get(url, function(data){
     PT.responses = data;
+    $(".response-count").html(data.length);
 
-    PT.minuteData = d3.nest()
+    PT.responsesPerDay = d3.nest()
       .key(function(d) {
-        return new Date(d.timestamp).setSeconds(0);
+        var day = new Date(d.timestamp).setHours(0, 0, 0, 0)
+        return new Date(day);
       })
       .entries(data);
+
+    PT.renderGraph(PT.responsesPerDay, '#activity-graph');
   });
 };
 
-PT.renderGraph = function(dataArray, containerId){
+PT.renderGraph = function(data, containerId){
 
   // Setup Variables //
+  var $container, height, width, margins, svg, graph, x, y, 
+      xAxis, yAxis, xExtene, line, startLine, plotPoints;
 
-  var width, height, pointSize, format, x, y, svg, graph, parseDate, data;
-  
-  width = $(containerId).width();
-  height = $(containerId).height();
+  $container = $(containerId);
+  height = 250;
+  width = $container.width();
+  margins = {
+    x: 60,
+    y: 40
+  };
 
-  x = d3.time.scale()
-      .range([0, width]);
+  svg = d3.select(containerId).append('svg')
+      .attr('width', width)
+      .attr('height', height);
 
-  y = d3.scale.linear()
-      .range([height - 20, 0]);
-
-  svg = d3.select(containerId)
-    .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(10, 10)");   
-    
   graph = svg.append('g')
-      .attr("class", "graph")
-      .attr("transform", "translate(0, 60)");
+      .attr("class", "graph-activity")
+      .attr("transform", "translate(30, 20)");
 
-  // Axes //
+  x = d3.time.scale().range([0, (width - margins.x)]);
+  y = d3.scale.linear().range([(height - margins.y), 0]);
 
-  var xAxis = d3.svg.axis()
+  xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom")
-      .tickSize(height - 20)
-      .tickPadding(20)
-      // .tickFormat(d3.time.format('%b'));
+      .tickSize(height - margins.y)
+      .tickPadding(10)
+      .ticks(d3.time.day, 1)
+      .tickFormat(d3.time.format('%d %b'));
 
-  var yAxis = d3.svg.axis()
+  yAxis = d3.svg.axis()
       .scale(y)
       .orient("left")
-      .tickSize(width)
-      .tickPadding(15)
+      .tickSize(width - margins.x)
+      .tickPadding(10)
       .ticks(5);
 
-  var line = d3.svg.line()
-      .x(function(d,i) { return x(new Date(parseInt(d.key)); })
-      .y(function(d) { return y(d.values.length); });
-
-  var startLine = d3.svg.line()
-      .x(function(d,i) { return x(new Date(d.key)); })
+  startLine = d3.svg.line()
+      .x(function(d) { return x(new Date(d.key)); })
       .y(height);
 
-    
-  // Legend //
+  line = d3.svg.line()
+      .x(function(d) { return x(new Date(d.key)); })
+      .y(function(d) { return y(d.values.length); });
 
-  // var renderLegend = function(){
-  //   var series = [
-  //     {name: "Your usage", colorClass: "statement"},
-  //     {name: "Average Arcadia member usage", colorClass: "average"}
-  //   ];
+  plotPoints = function(data){
+    graph.append("path")
+      .datum(data)
+      .attr("class", "activity-line")
+      .attr("d", startLine)
+      .transition()
+      .duration(700)
+      .attr("d", line);
 
-  //   var legendPos;
-  //   if(threshold){
-  //     legendPos = {height: 40, xPos: 150, yPos: function() {return 10;}};
-  //   } else {
-  //     legendPos = {height: 60, xPos: 0, yPos: function(i) {return 20 * i;}};
-  //   }
-
-  //   var legendBox = svg.append('g')
-  //       .attr("class", "legend")
-  //       .attr("transform", "translate(0, 10)")
-  //       .attr("height", legendPos.height)
-  //       .attr("width", 200);
-
-  //   legendBox.selectAll('g')
-  //       .data(series)
-  //     .enter().append('circle')
-  //       .attr("class", function(d) { return "point " + d.colorClass; })
-  //       .attr("r", pointSize)
-  //       .attr("stroke-width", pointSize / 1.5)
-  //       .attr("cy", function(d, i) { return legendPos.yPos(i); })
-  //       .attr("cx", function(d,i) { return legendPos.xPos * i; });
-
-  //   legendBox.selectAll('g')
-  //       .data(series)
-  //     .enter().append('text')
-  //       .attr("x", function(d,i) { return legendPos.xPos * i + 20; } )
-  //       .attr("y", function(d, i) { return legendPos.yPos(i) + 5; })
-  //       .text(function(d) { return d.name; });
-  //   };
-
-
-    // Data points //  
-
-    var plotPoints = function(dataArray){
-      // graph.append("path")
-      //     .datum(dataArray)
-      //     .attr("class", "line")
-      //     .attr("d", startLine)
-      //     .transition()
-      //     .duration(700)
-      //     .attr("d", line);
-
-      graph.append('g')
-          .attr("class", "data-points")
-          .selectAll('circle')
-        .data(dataArray)
-          .enter()
-          .append('circle')
+    graph.append("g")
+        .attr("class", "data-points")
+        .selectAll("circle")
+      .data(data)
+        .enter()
+        .append("circle")
           .attr("class", "point")
-          .attr("cx", function(d){return x(new Date(d.key));})
-          .attr("r", pointSize)
-          .attr("stroke-width", pointSize / 1.5)
+          .attr("r", 5)
+          .attr("cx", function(d) { return x(new Date(d.key)); })
+          .attr("cy", height)
           .transition()
           .duration(700)
-          .attr("cy", function(d) { return y(d.values.length);});
-    };
+          .attr("cy", function(d) { return y(d.values.length); })
+  };
 
+  if(data.length > 0) {
+    xExtent = d3.extent(data, function(d) { return new Date(d.key); });
+  } else {
+    xExtent = [new Date(Date.now()).setHours(0,0,0,0), new Date(Date.now()).setHours(0,0,0,0)];
+  }
+  x.domain([d3.time.day.offset(xExtent[0], -1), d3.time.day.offset(xExtent[1], 5)]);
+  y.domain([0, d3.max(data, function(d) { return d.values.length; }) + 10 || 10])
+  
+  graph.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0,0)")
+    .call(xAxis);
 
-    // Tooltip //
+  graph.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(0" + (width - margins.x) + ",0)")
+    .call(yAxis);
 
-    // var toolTip = d3.tip()
-    //     .attr('class', 'd3-tip')
-    //     .offset([-10, 0])
-    //     .html(function(d) { return d.kwh + " KWH"; });
+  plotPoints(data);
 
-
-    // Fetch data and render graph //
-       
-     // Set domain 
-      var xExtent = d3.extent(dataArray, function(d){return d.key;});
-      x.domain([new Date(xExtent[0]), new Date(xExtent[0])]);
-      y.domain([0, d3.max(dataArray, function(d) { return d.values.length })]);
-
-
-      // Create axes
-      graph.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0, 0)")
-        .call(xAxis);
-
-      graph.append("g")
-          .attr("class", "y axis")
-          .attr("transform", "translate(" + width + ", 0)")
-          .call(yAxis);
-        // .append("text")
-        // .text("KWH")
-        //   .attr("x", (-width -40))
-        //   .attr("y", -10)
-        //   .attr("class", "legend");
-
-      // // Plot data    
-      plotPoints(PT.minuteData);
-    
-      // Add legend & tooltip
-      // renderLegend();
-      // graph.call(toolTip);
 };
