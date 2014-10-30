4375e5b407d3edc84eb87368016037e636adad9e
Wang Yu <wangyu@WangmatoMacBook-Pro.local>
2014-10-30 18:08:08 -0400

    require 'net/http'

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require 'net/http'
 
 # Initialize the Rails application.
 PromiseTracker::Application.initialize!
