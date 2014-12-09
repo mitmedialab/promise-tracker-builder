7b022f67550633e5f602857bfeeecc0590e25d37
emreiser <emilie.reiser@gmail.com>
2014-12-09 17:30:34 -0500

    Disable turbolinks on full-width layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery-ui/draggable
+//= require jquery-ui/droppable
+//= require jquery-ui/sortable
 //= require turbolinks
 //= require handlebars
 //= require handlebars.runtime
