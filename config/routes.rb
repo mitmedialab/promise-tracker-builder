4eb50be17e0ecba6b11a79b8ce6a7caeda580e60
emreiser <emilie.reiser@gmail.com>
2014-12-11 11:35:54 -0500

    Add description to campaign overview

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,29 @@ PromiseTracker::Application.routes.draw do
   root to: 'home#index'
   match ':status', via: [:get, :post], to: 'errors#show', constraints: {status: /\d{3}/ }
 
-  get '/surveys/:id/preview', to: 'surveys#preview', as: 'preview_survey'
-  get '/surveys/test', to: 'surveys#test_builder', as: 'test_builder'
-  put '/surveys/:id/save-order', to: 'surveys#save_order', as: 'save_order'
-
-  get '/campaigns/:id/goals', to: 'campaigns#goals_wizard', as: 'campaign_goals_wizard'
-  get '/campaigns/:id/launch', to: 'campaigns#launch', as: 'launch_campaign'
-  get '/campaigns/:id/activate', to: 'campaigns#activate', as: 'activate_campaign'
-  get '/campaigns/:id/monitor', to: 'campaigns#monitor', as: 'monitor_campaign'
-  get '/campaigns/:id/share', to: 'campaigns#share', as: 'share_campaign'
-  get '/campaigns/:id/close', to: 'campaigns#close', as: 'close_campaign'
-  post '/campaigns/:id/clone', to: 'campaigns#clone', as: 'clone_campaign'
-  get '/campaigns/:id/setup', to: 'campaigns#setup', as: 'setup_campaign'
-
-  post '/inputs/:id/clone', to: 'inputs#clone', as: 'clone_input'
-
-  get '/download', to: 'home#download', as: 'download'
-
   scope "(:locale)", locale: /en|pt-BR/ do
     resources :users
     resources :campaigns
     resources :surveys do
       resources :inputs
     end
+
+    get '/surveys/:id/preview', to: 'surveys#preview', as: 'preview_survey'
+    get '/surveys/test', to: 'surveys#test_builder', as: 'test_builder'
+    put '/surveys/:id/save-order', to: 'surveys#save_order', as: 'save_order'
+
+    get '/campaigns/:id/goals', to: 'campaigns#goals_wizard', as: 'campaign_goals_wizard'
+    get '/campaigns/:id/launch', to: 'campaigns#launch', as: 'launch_campaign'
+    get '/campaigns/:id/activate', to: 'campaigns#activate', as: 'activate_campaign'
+    get '/campaigns/:id/monitor', to: 'campaigns#monitor', as: 'monitor_campaign'
+    get '/campaigns/:id/share', to: 'campaigns#share', as: 'share_campaign'
+    get '/campaigns/:id/close', to: 'campaigns#close', as: 'close_campaign'
+    post '/campaigns/:id/clone', to: 'campaigns#clone', as: 'clone_campaign'
+    get '/campaigns/:id/setup', to: 'campaigns#setup', as: 'setup_campaign'
+
+    post '/inputs/:id/clone', to: 'inputs#clone', as: 'clone_input'
+
+    get '/download', to: 'home#download', as: 'download'
   end
 
   namespace :api do
