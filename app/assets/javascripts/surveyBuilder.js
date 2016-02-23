405e7bbf1381f45787b21eaea98d771a7d0bfacf
emreiser <emilie.reiser@gmail.com>
2016-02-23 13:36:25 -0500

    Add google play badges

--- app/assets/javascripts/surveyBuilder.js
+++ app/assets/javascripts/surveyBuilder.js
@@ -51,7 +51,7 @@ PT.Input = function(){
     } 
   };
 
-  self.save = function(self, event){
+  self.save = function(self){
     $.ajax({
       url: Routes.survey_inputs_path(PT.survey.id),
       type: "POST",
@@ -60,15 +60,16 @@ PT.Input = function(){
       data: ko.toJSON(self)
     })
     .done(function(response) {
-      console.log(response);
       self.inEdit(false);
       self.options(response.options);
+      console.log("Input saved with label:" + self.label());
 
       if(self.id() === ""){
         self.id(response.id);
       }
       self.validate();
       PT.checkErrors();
+      PT.unsaved = false;
     });
   };
 
@@ -84,7 +85,7 @@ PT.Input = function(){
   }; 
 
   self.edit = function(){
-    PT.survey.saveInputs();
+    // PT.survey.saveInputs();
     self.inEdit(true);
     PT.selectedInput(self);
   };
@@ -133,35 +134,29 @@ PT.SurveyModel = function(){
     event.stopPropagation();
     self.saveInputs();
 
-    var input = new PT.Input();
-    var type = PT.defaultControls[$(event.currentTarget).attr("rel")];
-    var index;
-    
-    if(type){
-      input.input_type(type["input_type"]);
-    }
+    PT.waitForAllSaved(function(){
+      var input = new PT.Input();
+      self.inputs.push(input);
+      console.log("new input added"); // DEBUG
+      PT.selectedInput(input);
 
-    if($(event.target).hasClass("drag-insert")){
-      index = $(event.target).index();
-      $(event.target).remove();
-    } else {
-      index = self.inputs().length;
-    }
-
-    self.inputs.splice(index, 0, input);
-    PT.selectedInput(input);
-    if($(".selected").length > 0) {
-      $(".selected").find("input")[0].focus();
-    }
+      var label = $(".selected").find("textarea")[0];
+      if(label){
+        label.focus();
+      }
+    });
   };
 
   self.saveInputs = function(){
-    var unsaved = self.inputs().filter(function(input) { return input.inEdit() === true ;});
-    unsaved.forEach(function(input){input.save(input); });
+    PT.unsaved = self.inputs().filter(function(input) { return input.inEdit() === true ;})[0] || false;
+    if(PT.unsaved){
+      console.log("Unsaved stored as: " + PT.unsaved.label());
+      PT.unsaved.save(PT.unsaved);
+    }
   };
 
   self.removeInput = function(){
-    var confirmed = window.confirm(I18n.t("surveys.survey_builder.confirm_input_delete"));
+    var confirmed = this.label() === "" || window.confirm(I18n.t("surveys.survey_builder.confirm_input_delete"));
     if(confirmed){
       self.inputs.remove(this);
       if(this.id()){
@@ -183,18 +178,21 @@ PT.SurveyModel = function(){
   /// Update order of all inputs
   self.saveOrder = function(){
     self.saveInputs();
-    
-    $.ajax({
-      url: Routes.save_order_path(PT.survey.id),
-      type: "PUT",
-      contentType: "application/json",
-      dataType: "json",
-      data: ko.toJSON(self)
-    })
-    .done(function(data){
-      window.location.pathname = I18n.currentLocale() + Routes[data.redirect_path](self.campaign_id);
-    });
 
+    var store = function(){
+      $.ajax({
+        url: Routes.save_order_path(PT.survey.id),
+        type: "PUT",
+        contentType: "application/json",
+        dataType: "json",
+        data: ko.toJSON(self)
+      })
+      .done(function(data){
+        window.location.pathname = I18n.currentLocale() + Routes[data.redirect_path](self.campaign_id);
+      });
+    };
+
+    PT.waitForAllSaved(store);
   };
 
   self.populateInputs = function(data){
@@ -257,5 +255,16 @@ PT.checkErrors = function(){
   } else {
     $("#error-check").removeClass("alert-danger");
     $("#error-check p").html(I18n.t("defaults.validations.no_errors"));
+  }
+};
+
+PT.waitForAllSaved = function(callback){
+  if(!PT.unsaved){
+    callback();
+    return;
+  } else {
+    window.setTimeout(function(){
+      PT.waitForAllSaved(callback);
+    }, 100);
   }
 };
