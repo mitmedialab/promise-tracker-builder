aa62317ebcae6403ec3edddb074740000d0b2a44
emreiser <emilie.reiser@gmail.com>
2017-10-11 13:09:14 -0400

    Use blob for CSV download

--- app/assets/javascripts/csvDownload.js
+++ app/assets/javascripts/csvDownload.js
@@ -8,14 +8,14 @@ PT.downloadCsv = function(serverResponse){
 
   //Write title & prompts
   csvString += survey.title + "\n";
-  csvString += 'Date of submission,"Location of submission (lat, lon)",';
+  csvString += '"Date of submission","Location of submission (lat, lon)",';
   survey.inputs.forEach(function(input){
     csvString += '"' + input.label + '",';
   })
 
   //Write responses
   responses.forEach(function(response){
-    csvString += "\n" + new Date(response.timestamp) + "," 
+    csvString += "\n" + '"' + new Date(response.timestamp) + '",'
       + '"' + response.locationstamp.lat + ", " + response.locationstamp.lon + '",';
 
     response.answers.forEach(function(answer){
@@ -24,17 +24,29 @@ PT.downloadCsv = function(serverResponse){
           csvString += '"' + answer.value + '"';
         } else if(answer.value.constructor == Array) {
           csvString += '"' + answer.value.join(",") + '"';
-        } else {
+        } else if (typeof(answer.value) == "object") {
           csvString += '"' + answer.value.lat + ", " + answer.value.lon + '"';
         }
       }
       csvString += ",";
     })
-  })
+  });
 
-  a.href = "data:application/csv;charset=utf-8," + "\uFEFF" + encodeURIComponent(csvString);
-  a.target = "_blank";
-  a.download = "PT_Data_" + survey.code + ".csv";
-  document.body.appendChild(a);
-  a.click();
+  var filename = "PT_Data_" + survey.code + ".csv";
+  var blob = new Blob(["\uFEFF" + csvString], { encoding:"UTF-8", type: 'text/csv' });
+  if (navigator.msSaveBlob) { // IE 10+
+    navigator.msSaveBlob(blob, filename);
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) { // feature detection
+      // Browsers that support HTML5 download attribute
+      var url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", filename);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
 };
