ceb52abb2fc40e27e1bebe54389e453e8fd9d423
emreiser <emilie.reiser@gmail.com>
2018-03-09 13:05:27 -0500

    Undo update gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui/widgets/draggable
-//= require jquery-ui/widgets/droppable
-//= require jquery-ui/widgets/sortable
+//= require jquery-ui/draggable
+//= require jquery-ui/droppable
+//= require jquery-ui/sortable
 //= require jquery.ui.touch-punch
-//= require jquery-ui/widgets/slider
+//= require jquery-ui/slider
 //= require turbolinks
 //= require handlebars
 //= require handlebars.runtime
